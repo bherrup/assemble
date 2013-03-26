231b6d3ed054c8f747f3505e890e5d1b7f5e691b
Bradley Herrup <bherrup@gmail.com>
2013-03-26 00:51:45 -0400

    update styles

--- src/assemble/views/welcome.clj
+++ src/assemble/views/welcome.clj
@@ -6,12 +6,18 @@
 
 (defpage "/" []
   (common/layout 
-    [:div {:class "title"} "assemble"]
-    [:div {:class "subtitle"} "your workplace, evolved"]
-    [:div {:class "mission-statement"} 
-      [:p "We believe that thoughtfully designed & serviced workplaces enhance productivity, inspire worker satisfaction and
-           ultimately contribute to exceptional business performance. It is with this focus that we provide professional work 
-           environments for a curated community of likeminded members and do so with pricing that allows us to thrive together."]
-      [:p "We invite our members to let us know if we deviate from our path, knowing that shared success is a team effort."]
+    [:div {:id "title"} 
+      [:div {:class "title-placement"}
+        [:div {:class "title-text"} "assemble"]
+      ]
+    ]
+    [:div {:id "subtitle"} "your workplace, evolved"]
+    [:div {:id "mission-statement"}
+      [:div {:class "mission"}
+        [:p "We believe that thoughtfully designed & serviced workplaces enhance productivity, inspire worker satisfaction and
+             ultimately contribute to exceptional business performance. It is with this focus that we provide professional work 
+             environments for a curated community of likeminded members and do so with pricing that allows us to thrive together."]
+        [:p "We invite our members to let us know if we deviate from our path, knowing that shared success is a team effort."]
+      ] 
     ]
     ))
