f2da8a7bffafb262399ce147e3ce19070d6b9f1c
Bradley Herrup <bherrup@gmail.com>
2013-03-26 01:14:36 -0400

    update project

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
             :description "Assemble Web Page"
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.3.0-beta3"]]
+            :min-lein-version "2.0.0"
             :main assemble.server)
 
