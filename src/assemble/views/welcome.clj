b34a0d05b292aafe3479f6c6bc085141532ca90d
Bradley Herrup <bherrup@gmail.com>
2013-04-02 09:39:41 -0400

    update text and styles

--- src/assemble/views/welcome.clj
+++ src/assemble/views/welcome.clj
@@ -5,21 +5,27 @@
         [hiccup.core]))
 
 (defpage "/" []
-  (common/layout 
-    [:div {:id "title"} 
+  (common/layout
+    [:div {:id "title"}
       [:div {:class "title-placement"}
         [:h1 {:class "title-text"} "assemble"]
       ]
     ]
-    [:div {:id "subtitle"} 
+    [:div {:id "subtitle"}
       [:div {:class "subtitle-text"}
-        "your workplace, evolved"]]
+        "where work comes together"]]
     [:div {:id "mission-statement"}
       [:div {:class "mission"}
         [:p "We believe that thoughtfully designed & serviced workplaces enhance productivity, inspire worker satisfaction and
-             ultimately contribute to exceptional business performance. It is with this focus that we provide professional work 
+             ultimately contribute to exceptional business performance. It is with this focus that we provide professional work
              environments for a curated community of likeminded members and do so with pricing that allows us to thrive together."]
         [:p "We invite our members to let us know if we deviate from our path, knowing that shared success is a team effort."]
-      ] 
+      ]
+    ]
+    [:div {:id "footer"}
+      [:p "Coming Soon to Arlington, VA "
+        [:br]
+        [:a {:href "mailto:info@assemble.com"} "Contact Us"]
+      ]
     ]
     ))
